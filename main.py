0432618efbb2b4990723209f2045c9f427584825
kosuke miyoshi <miyoshi@narr.jp>
2018-10-01 16:51:05 +0900

    fix indent

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ import cv2
 
 
 class MovieWriter(object):
-  def __init__(self, file_name, frame_size, fps):
-    """
-    frame_size is (w, h)
-    """
-    self._frame_size = frame_size
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    self.vout = cv2.VideoWriter()
-    success = self.vout.open(file_name, fourcc, fps, frame_size, True)
-    if not success:
-        print("Create movie failed: {0}".format(file_name))
+    def __init__(self, file_name, frame_size, fps):
+        """
+        frame_size is (w, h)
+        """
+        self._frame_size = frame_size
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.vout = cv2.VideoWriter()
+        success = self.vout.open(file_name, fourcc, fps, frame_size, True)
+        if not success:
+            print("Create movie failed: {0}".format(file_name))
 
-  def add_frame(self, frame):
-    """
-    frame shape is (h, w, 3), dtype is np.uint8
-    """
-    self.vout.write(frame)
+    def add_frame(self, frame):
+        """
+        frame shape is (h, w, 3), dtype is np.uint8
+        """
+        self.vout.write(frame)
 
-  def close(self):
-      self.vout.release() 
-      self.vout = None
+    def close(self):
+        self.vout.release() 
+        self.vout = None
 
 
 CELL_W_SIZE = 64
